fce44aaeb4130a429516bdfd69a5fd4b69e1c977
Michael.Erickson <ylnorthville@gmail.com>
2020-11-05 11:48:56 -0500

    Delete Functions and Added Other functions as well

--- wall_app/models.py
+++ wall_app/models.py
@@ -29,8 +29,12 @@ class User(models.Model):
 class Message(models.Model):
     message=models.CharField(max_length=255)
     person = models.ForeignKey(User, related_name='user_messages', on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
     
 class Comment(models.Model):
     comment=models.CharField(max_length=255)
     person = models.ForeignKey(User, related_name='user_comments', on_delete=models.CASCADE)
     wall_message = models.ForeignKey(Message, related_name="post_comments", on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
